7ff9409abe6eddf0f2a85bb863fb4d1fdebde132
ALX <alnikitin2005@gmail.com>
2025-08-22 16:26:50 +0300

    Initial Commit

---
+++ src/main/java/net/alx/magicwand/MagicWand.java
@@ -0,0 +1,15 @@
+package net.alx.magicwand;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicWand implements ModInitializer {
+	public static final String MOD_ID = "magic-wand";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/alx/magicwand/MagicWandClient.java
@@ -0,0 +1,10 @@
+package net.alx.magicwand;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicWandClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
